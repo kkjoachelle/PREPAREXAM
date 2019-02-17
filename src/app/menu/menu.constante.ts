a274ff28258383327b1ebf867245ec0ead5b2441
kalain20 <kalain20@yahoo.fr>
2019-02-17 18:07:48 -0500

    Amelioration du menu

--- src/app/menu/menu.constante.ts
+++ src/app/menu/menu.constante.ts
@@ -2,9 +2,11 @@ export class Constantes{
     public readonly menu: string = "menu";
     public readonly home: string = "accueil";
     public readonly about: string = "à propos";
+    public readonly partenaires: string = "partenaires";
     public readonly advice: string = "conseils";
-    public readonly contact:string = "contactez-nous";
-    public readonly subscribe:string = "inscription";
-    public readonly connexion:string = "connexion";
-    
+    public readonly contact: string = "contactez-nous";
+    public readonly subscribe: string = "inscription";
+    public readonly blog: string = "blog";
+    public readonly connexion: string = "connexion / inscription";
+     
 }
